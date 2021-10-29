d0811962513406d553b6226d4adaa5456179bf59
Xithrius <xithrius@gmail.com>
2021-10-28 21:06:31 -0700

    Reddit requests are now functioning

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,61 @@
 use anyhow::Result;
 
 use crate::handlers::config::CompleteConfig;
-use crate::utils::pathing::config_path;
 
 mod handlers;
 mod reddit;
 mod terminal;
 mod utils;
 
-const CONF: &str = "https://github.com/Xithrius/terminal-reddit/blob/main/default-config.toml";
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    if let Ok(config_contents) = std::fs::read_to_string(config_path()) {
-        let config: CompleteConfig = toml::from_str(config_contents.as_str())?;
+    match CompleteConfig::new() {
+        Ok(config) => {
+            // <platform>:<app ID>:<version string> (by /u/<reddit username>)
+            let user_agent = format!("tui/v{} by {}", env!("CARGO_PKG_VERSION"), config.username);
 
-        // <platform>:<app ID>:<version string> (by /u/<reddit username>)
-        let user_agent = format!(
-            "{}/v{} by Xithrius",
-            env!("CARGO_PKG_NAME"),
-            env!("CARGO_PKG_VERSION")
-        );
+            println!("{}", user_agent);
 
-        let client = reqwest::Client::builder().build().unwrap();
+            let client = reqwest::Client::builder().build().unwrap();
 
-        let form = reqwest::multipart::Form::new()
-            .text("User-Agent", user_agent.to_string())
-            .text("grant_type", "password")
-            .text("username", config.username.to_string())
-            .text("password", config.password.to_string());
+            let form = reqwest::multipart::Form::new()
+                .text("User-Agent", user_agent.to_string())
+                .text("grant_type", "password")
+                .text("username", config.username.to_string())
+                .text("password", config.password.to_string());
 
-        let response = client
-            .post("https://www.reddit.com/api/v1/access_token")
-            .multipart(form)
-            .basic_auth(
-                config.client_id.to_string(),
-                Some(config.client_secret.to_string()),
-            )
-            .send()
-            .await
-            .unwrap();
-
-        if response.status() != 200 {
-            println!(
-                "{}",
-                format!(
-                    "Could not authenticate through the Reddit API. Status: {}.",
-                    response.status()
+            let response = client
+                .post("https://www.reddit.com/api/v1/access_token")
+                .multipart(form)
+                .basic_auth(
+                    config.client_id.to_string(),
+                    Some(config.client_secret.to_string()),
                 )
-            );
-        } else {
-            let some_json: serde_json::Value = response.json().await.unwrap();
+                .send()
+                .await
+                .unwrap();
 
-            let token = format!("bearer {}", some_json["access_token"]);
+            if response.status() != 200 {
+                println!(
+                    "{}",
+                    format!(
+                        "Could not authenticate through the Reddit API. Status: {}.",
+                        response.status()
+                    )
+                );
+            } else if let Ok(data) = response.json().await {
+                let auth_info: serde_json::Value = data;
 
-            let request_form = reqwest::multipart::Form::new()
-                .text("Authorization", token)
-                .text("User-Agent", user_agent);
+                let token = format!("bearer {}", auth_info["access_token"]).replace("\"", "");
 
-            let posts = reddit::get_reddit_posts(client, request_form, config.clone()).await;
+                let posts = reddit::get_reddit_posts(client, token, user_agent, config).await;
 
-            for post in posts {
-                println!("{:?}", post);
+                terminal::draw_terminal_ui().await;
             }
         }
-
-        // terminal::draw_terminal_ui().await.unwrap();
-    } else {
-        println!(
-            "Configuration not found. Create a config file at '{}', and see '{}' for an example configuration.",
-            config_path(),
-            CONF,
-        );
+        Err(err) => {
+            println!("{}", err);
+        }
     }
 
     Ok(())
--- src/reddit.rs
+++ src/reddit.rs
@@ -3,9 +3,9 @@ use std::collections::VecDeque;
 use reqwest::Client;
 use serde::Deserialize;
 
-use crate::handlers::config::CompleteConfig;
+use anyhow::{bail, Error, Result};
 
-const BASE_URL: &str = "https://www.reddit.com";
+use crate::handlers::config::CompleteConfig;
 
 #[derive(Deserialize, Debug)]
 pub struct Post {
@@ -13,50 +13,59 @@ pub struct Post {
     pub title: String,
     pub url: String,
     pub permalink: String,
+    pub image_url: String,
 }
 
 impl Post {
-    pub fn new(author: String, title: String, url: String, permalink: String) -> Self {
+    pub fn new(author: String, title: String, url: String, permalink: String, image_url: String) -> Self {
         Post {
             author,
             title,
             url,
             permalink,
+            image_url,
         }
     }
 }
 
 pub async fn get_reddit_posts(
     client: Client,
-    request_form: reqwest::multipart::Form,
+    token: String,
+    user_agent: String,
     config: CompleteConfig,
-) -> VecDeque<Post> {
-    let response: serde_json::Value = client
-        .get(format!(
-            "{}/r/{}/.json",
-            BASE_URL,
-            config.subreddit.to_string()
-        ))
-        .multipart(request_form)
+) -> Result<VecDeque<Post>, Error> {
+    let url = format!(
+        "https://oauth.reddit.com/r/{}/{}/?t={}.json",
+        config.subreddit, config.status, config.timespan
+    );
+
+    let response = client
+        .get(url)
+        .header("Authorization", token)
+        .header("User-Agent", user_agent)
         .send()
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
+        .await;
 
-    let mut posts = VecDeque::new();
+    match response {
+        Ok(data) => {
+            let info: serde_json::Value = data.json().await.unwrap();
 
-    for item in response["data"]["children"].as_array().unwrap() {
-        let tmp = &item["data"];
+            let mut posts = VecDeque::new();
 
-        posts.push_front(Post::new(
-            tmp["author"].to_string(),
-            tmp["title"].to_string(),
-            tmp["url"].to_string(),
-            tmp["permalink"].to_string(),
-        ));
+            for item in info["data"]["children"].as_array().unwrap() {
+                let tmp = &item["data"];
+
+                posts.push_front(Post::new(
+                    tmp["author"].to_string(),
+                    tmp["title"].to_string(),
+                    tmp["url"].to_string(),
+                    format!("https://reddit.com{}", tmp["permalink"].to_string()),
+                    tmp["url"].to_string(),
+                ));
+            }
+
+            Ok(posts)
+        }
+        Err(err) => bail!(format!("Request to Reddit API failed. Error: {}", err)),
     }
-
-    posts
 }
